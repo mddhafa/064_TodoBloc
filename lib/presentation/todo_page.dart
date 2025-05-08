af6f6699b7e1b46ca67327f15f53808bbfd5eb12
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-08 23:12:47 +0700

    Membuat judul todo di dalam item list

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -118,6 +118,23 @@ class TodoPage extends StatelessWidget {
                               color: Colors.blue[50],
                               borderRadius: BorderRadius.circular(8.0),
                             ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      todo.title,
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
                           );
                         },
                       );
