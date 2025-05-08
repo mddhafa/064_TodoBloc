07f8d15e6197d9e0fc03b056a55204a3d0cbf05c
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-05-08 23:24:50 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:todo_app/presentation/todo_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoPage(),
     );
   }
 }
